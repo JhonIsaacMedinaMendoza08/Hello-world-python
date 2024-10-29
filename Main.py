37de98676d0d20ee71279495fe0bde69760d1421
jhonIsaacMedinaMendoza08 <186082473+JhonIsaacMedinaMendoza08@users.noreply.github.com>
2024-10-29 12:05:14 -0500

    Las tres partes principales de un algoritmo

--- Main.py
+++ Main.py
@@ -1,21 +1,4 @@
-name= "Jhon Isaac Medina Mendoza"    #Tipos de datos string
-gender = "M"  #Tipo de dato caracter
-age = 23   #tipo de dato int
-height = 1.72    #tipo de dato doble
-trainer = True    # tipo de dato boolean
-
-# Conjuntos
-hobbies = ["cocinar", 'programar', """wow Horda"""]      #TIpo de dato ARRAY
-direction = (dict) ["country": "Colombia", "State": "Santander"]   #Tipo de dato object
-
-print(f"""
-      Student name: {name}   
-      Student age {age}
-      Student gender: {gender}
-      Student height: {height}  
-    
-""")
-
-#lo que no se puede hacer con los tipos de datos
-#combinaciones = "(a1)(/&)*-+"
-#nombre 1 = "Isaac"   Debe de ir así "nombre_1" o "FirstName"
+fullname= input("Enter yout full name: ")
+age = int(input ("Enter your age: "))
+gender = (input ("Enter your possible gender options M, F, X: "))
+height =float(input("Enter your height: "))
