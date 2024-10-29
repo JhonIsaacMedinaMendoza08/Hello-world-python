27834cd56ae84acbfef0110003d0cc1acd98f869
jhonIsaacMedinaMendoza08 <186082473+JhonIsaacMedinaMendoza08@users.noreply.github.com>
2024-10-29 11:59:12 -0500

    Obtener datos del usuario

--- Main.py
+++ Main.py
@@ -1,3 +1,21 @@
-name= input("Enter your name: ")
-Message = f"Hello World! {name}" 
-print(Message) 
+name= "Jhon Isaac Medina Mendoza"    #Tipos de datos string
+gender = "M"  #Tipo de dato caracter
+age = 23   #tipo de dato int
+height = 1.72    #tipo de dato doble
+trainer = True    # tipo de dato boolean
+
+# Conjuntos
+hobbies = ["cocinar", 'programar', """wow Horda"""]      #TIpo de dato ARRAY
+direction = (dict) ["country": "Colombia", "State": "Santander"]   #Tipo de dato object
+
+print(f"""
+      Student name: {name}   
+      Student age {age}
+      Student gender: {gender}
+      Student height: {height}  
+    
+""")
+
+#lo que no se puede hacer con los tipos de datos
+#combinaciones = "(a1)(/&)*-+"
+#nombre 1 = "Isaac"   Debe de ir así "nombre_1" o "FirstName"
